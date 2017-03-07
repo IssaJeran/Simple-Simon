9c21b990a46359d1b3f13dfbca5dd2babc5ae382
IssaJeran <jeransmith22@gmail.com>
2017-03-07 09:32:04 -0600

    // Added final Simple Simon js file

--- public/simplesimon.js
+++ public/simplesimon.js
@@ -1,15 +1,15 @@
-"use strict";
-(function(){
-
 $(document).ready(function() {
+
+	"use strict";
 
 	var div = ["pink","green","orange","blue"];
 	var lights = [];
 	var	random;
 	var position = 0;
 	var counter = 0;
-	var rightAnswer = new Audio('file:///Users/jeransmith/vagrant-lamp/sites/simplesimon.dev/public/gameover.wav');
-	var wrongAnswer = new Audio('file:///Users/jeransmith/vagrant-lamp/sites/simplesimon.dev/public/gameover.wav');
+	var wrongAnswer1 = new Audio("audiocombine1.mp3");
+	var kanye = new Audio("kanyejoin.mp3")
+		kanye.play();
 
 	//Begins the game
 	$('#begin').click(function (){
@@ -29,12 +29,11 @@ $(document).ready(function() {
 		$('#roundTracker').html(counter);
 	}
 
-	//Flashes Simon sequence. 
+	//Flashes Simon sequence 
 	function buttonAnimate (){
 		lights.forEach(function(element,index){
 			var delay = 1000;
 			var flash = setTimeout(function(){	
-				rightAnswer.play();
 				$("#" + lights[index]).animate({
 					opacity: .25
 				},500).animate({
@@ -49,23 +48,21 @@ $(document).ready(function() {
 		random = div[Math.floor(Math.random()* 4)];
 	}
 
-	//Resets game to Start Over If Player Desires 
+	//Resets game to start over if player desires 
 	function reset(){
 		position = 0;
 		counter = 0;
 		roundTotal(counter);
 	}
 
-
-	//Compares what the user clicks and the array of lights
+	//Compares what the user clicks and the array of lights [pink, green, orange, blue]
 	$('#area').click(function(event){         
 		var target = $(event.target).attr('id');
 		if (target == lights[position]) {
-			rightAnswer.play();
 			position++;  
 		}   else {
-			wrongAnswer.play();
-			alert("Game Over But Continue Enjoying The Song! And Try Again!");
+			wrongAnswer1.play();
+			alert("Game Over But Continue Enjoying The Song You Unlocked! And Try Again!");
 			reset(); 
 		}
 		if (position == lights.length) { 
@@ -80,7 +77,7 @@ $(document).ready(function() {
 	});
 
 });
-})();
+
 
 
 
